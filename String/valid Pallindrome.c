1b1d9b49af0b0bba744cbceb1c454b4e68774581
Anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-06-22 20:05:16 +0545

    :package: Valid palindrome NUmber
    To check whether given number is pallindrom number or not

---
+++ String/valid Pallindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	int number,sum,remainder,temp;
+	printf("Enter the number : ");
+	scanf("%d",&number);
+	temp=number;
+	
+	while(number>0){
+		remainder=number%10;
+		sum=remainder+(sum*10);
+		number=number/10;
+	}
+	if(sum==temp){
+		printf("Pallindrome number");
+	}else {
+		printf("NOt pallindrome number ");
+	}
+	return 0;
+}
