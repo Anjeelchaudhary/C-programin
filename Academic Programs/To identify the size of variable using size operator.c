e37026938b5d6aa62f1b6859b7e05b2278bc245c
Anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-06-26 16:58:51 +0545

    :package: Size of variable
    Size of the variable using size operator

---
+++ Academic Programs/To identify the size of variable using size operator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	int w;
+	float x;
+	double y;
+	char z;
+	
+	printf("The size of integer is : %d \n",sizeof(w));
+	printf("The size of float is : %d \n ",sizeof(x));
+	printf("The size of double is : %d \n",sizeof(y));
+	printf("The size of char is : %d \n",sizeof(z));
+	getch();
+}
